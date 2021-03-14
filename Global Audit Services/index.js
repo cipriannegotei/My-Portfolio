ba05439a708a67872dde2ed52579ee2a3da4e4f9
cipriannegotei <65068222+cipriannegotei@users.noreply.github.com>
2021-03-14 20:12:20 +0200

    Add files via upload

---
+++ Global Audit Services/index.js
@@ -0,0 +1,18 @@
+function Header() {
+    document.querySelector(".hamburger")
+    .addEventListener("click", toggle)
+  
+    function toggle() {
+    var menu = document.querySelector("header ul")
+  
+    if (menu.classList.contains("hidden")) {
+      menu.classList.add("visible")
+      menu.classList.remove("hidden")
+    } else {
+      menu.classList.add("hidden")
+      menu.classList.remove("visible")
+    }
+  }
+  }
+  
+  Header() 
